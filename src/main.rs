cc0db87bdaef76fd11646dd121e47c54ab72a5d0
pommicket <pommicket@gmail.com>
2023-01-22 17:32:42 -0500

    start analysis

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 @TODO:
 - screenshot
 - RnToRn functions (& add back in RToR)
+ -  also add PerComponent(Box<RToR>,Box<RToR>,Box<RToR>) in R3ToR3
+- ProjectX, ProjectY, ProjectZ in R3ToR?
 - let user go back&forth through past sdfs using scenes.txt file
 - documentation
 - GenRandom integers (just use 0..u32::MAX and add a modulus)
